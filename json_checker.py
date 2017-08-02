637f4ccfd6736ae435c74281d279ae45f1c35c37
Denis Korytkin <d.korytkin@smartweb.com.ua>
2017-08-02 16:25:43 +0300

    Added validation dict data

--- json_checker.py
+++ json_checker.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 
 ERROR = namedtuple('Error', ['current_type', 'expected_type', 'current_value'])
 ERROR_TEMPLATE = 'current type {}, expected type {}, current value {}'
+DICT_ERROR_TEMPLATE = 'From key="{}"\n\t{}'
 REPR_TEMPLATE = u'{class_name}({current})'
 
 
@@ -17,11 +18,11 @@ class JsonCheckerException(Exception):
         return '\n{}'.format('\n'.join(self.errors))
 
 
-class ListValidator(object):
+class BaseChecker(object):
 
-    def __init__(self, *args, **kwargs):
-        self.data = args
-        self.errors = kwargs.get('errors', [])
+    def __init__(self, data):
+        self.data = data
+        self.errors = []
 
     def __repr__(self):
         return REPR_TEMPLATE.format(
@@ -31,10 +32,16 @@ class ListValidator(object):
 
     def _format_errors(self):
         if self.errors:
-            return ['ListCheckerErrors:\n{}'.format('\n'.join(self.errors))]
+            return '{}Errors:\n{}'.format(
+                self.__class__.__name__,
+                '\n'.join(self.errors)
+            )
+
+
+class ListChecker(BaseChecker):
 
     def validate(self, expected_data):
-        for checker in [Validator(d, errors=self.errors) for d in self.data]:
+        for checker in [Validator(d) for d in self.data]:
             result = checker.validate(expected_data)
             if result:
                 self.errors.extend(result)
@@ -42,22 +49,12 @@ class ListValidator(object):
         return self._format_errors()
 
 
-class TypeValidator(object):
+class TypeChecker(BaseChecker):
 
-    def __init__(self, data, **kwargs):
-        self.data = data
-        self.errors = kwargs.get('errors', [])
-
-    def __repr__(self):
-        return REPR_TEMPLATE.format(
-            class_name=self.__class__.__name__,
-            current=self.data
-        )
-
-    def _format_error(self):
+    def _format_errors(self):
         if self.errors:
             errors = [ERROR_TEMPLATE.format(*e) for e in self.errors]
-            return ['TypeCheckerError: {}'.format('\n'.join(errors))]
+            return 'TypeCheckerError: {}'.format('\n'.join(errors))
 
     def validate(self, expected_data):
         try:
@@ -70,14 +67,35 @@ class TypeValidator(object):
             self.errors.append(
                 ERROR(type(self.data), type(expected_data), e.__str__())
             )
-        return self._format_error()
+        return self._format_errors()
+
+
+class DictChecker(BaseChecker):
+
+    def _check_dicts(self, expected_dict):
+        assert expected_dict, 'Wrong expected dict is None'
+        assert self.data, 'Wrong current dict is None'
+        assert isinstance(expected_dict, dict), 'Expected data is not dict'
+        check_keys = set(self.data.keys()) ^ set(expected_dict.keys())
+        assert not check_keys, 'Difference keys {}'.format(check_keys)
+
+    def validate(self, expected_data):
+        self._check_dicts(expected_data)
+        for key, value in self.data.items():
+            checker = Validator(value)
+            result = checker.validate(expected_data.get(key))
+            if result:
+                self.errors.append(
+                    DICT_ERROR_TEMPLATE.format(key, '\n'.join(result))
+                )
+        return self._format_errors()
 
 
 class Validator(object):
 
-    def __init__(self, current_data, errors=None):
+    def __init__(self, current_data):
         self.data = current_data
-        self.errors = errors or []
+        self.errors = []
 
     def __repr__(self):
         return REPR_TEMPLATE.format(
@@ -85,12 +103,17 @@ class Validator(object):
             current=self.data
         )
 
+    def _append_errors(self, result):
+        if result:
+            self.errors.append(result)
+
     def validate(self, expected_data):
         if isinstance(self.data, (list, tuple, set, frozenset)):
-            list_checker = ListValidator(*self.data, errors=self.errors)
+            assert expected_data, 'Wrong expected data is None'
+            list_checker = ListChecker(self.data)
             try:
                 for ex_d in expected_data:
-                    return list_checker.validate(ex_d)
+                    self._append_errors(list_checker.validate(ex_d))
             except TypeError as e:
                 self.errors.append(
                     'TypeError: ' + ERROR_TEMPLATE.format(
@@ -98,12 +121,11 @@ class Validator(object):
                     )
                 )
         elif isinstance(self.data, dict):
-            pass
+            dict_checker = DictChecker(self.data)
+            self._append_errors(dict_checker.validate(expected_data))
         elif isinstance(self.data, (str, int)):
-            type_checker = TypeValidator(self.data, errors=self.errors)
-            result = type_checker.validate(expected_data)
-            return result
-
+            type_checker = TypeChecker(self.data)
+            self._append_errors(type_checker.validate(expected_data))
         return self.errors
 
 
