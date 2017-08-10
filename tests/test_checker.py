e2c63eb018e65a15c31e291f870c909be1971755
Denis Korytkin <d.korytkin@smartweb.com.ua>
2017-08-10 16:09:40 +0300

    added negative checker tests

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from checker import Checker, And, Or, OptionalKey
+from checker import Checker, And, Or, OptionalKey, SUPPORT_ITER_OBJECTS
 from checker_exceptions import (
     CheckerError,
     TypeCheckerError,
@@ -33,13 +33,54 @@ CHECKER_DATA_POSITIVE = [
     [{'key1': {'key2': {'key3': bool}}}, {'key1': {'key2': {'key3': False}}}],
     [{OptionalKey('key1'): int, 'key2': str}, {'key2': '123'}],
     [{OptionalKey('key1'): int, 'key2': str}, {'key1': 123, 'key2': '123'}],
-    # [{'key1': Or(int, None)}, {'key1': 123}],
+    [{'key1': Or(int, None)}, {'key1': 123}],
     [{'key1': Or(int, None)}, {'key1': None}],
+    [{'key1': And(int, lambda x: 0 < x < 99)}, {'key1': 30}],
+    [{'key1': And(list, lambda x: len(x) < 99)}, {'key1': list(range(98))}],
+    [{'key1': And(list, lambda x: len(x) < 99)}, {'key1': list([])}],
+    [{'key1': And(int, bool)}, {'key1': True}],
+    [{'key1': And(str, lambda x: x in ('t', 'e', 's', 't'))}, {'key1': 's'}],
 ]
 CHECKER_DATA_NEGATIVE = [
-    [int, True, '5', CheckerError],
-    [int, False, '5', TypeCheckerError],
+    [int, '5'],
+    [bool, 1],
+    [str, True],
+    [dict, 12],
+    [list, {'key': 1}],
+    [tuple, True],
+    [frozenset, 'test'],
+    [set, []],
+    [[int], ['test']],
+    [[bool], [1, 2, 3]],
+    [[str], list(range(1000))],
+    [[int], ['1'] * 1000],
+    [[bool], [1, False]],
+    [{'key1': int}, {'key1': '1'}],
+    [{'key1': int, 'key2': str, 'key3': bool},
+     {'key1': 666, 'key2': 123, 'key3': True}],
+    [{'key1': {'key2': str}}, {'key1': {'key2': 123}}],
+    [{'key1': {'key2': {'key3': bool}}}, {'key1': {'key2': {'key3': 1}}}],
+    [{OptionalKey('key1'): int, 'key2': str}, {'key2': 123}],
+    [{OptionalKey('key1'): int, 'key2': str}, {'key1': '123', 'key2': 123}],
+    [{'key1': Or(int, None)}, {'key1': '123'}],
+    [{'key1': Or(int, None)}, {'key1': 'True'}],
+    [{'key1': And(int, lambda x: 0 < x < 99)}, {'key1': 99}],
+    [{'key1': And(int, lambda x: 0 < x < 99)}, {'key1': 0}],
+    [{'key1': And(list, lambda x: len(x) < 99)}, {'key1': list(range(99))}],
+    [{'key1': And(int, bool)}, {'key1': None}],
+    [{'key1': And(str, lambda x: x in ('t', 'e', 's', 't'))}, {'key1': '1'}],
 ]
+
+
+def _get_expected_exception(ex_object, soft=False):
+    if isinstance(ex_object, SUPPORT_ITER_OBJECTS) and not soft:
+        return ListCheckerError
+    elif isinstance(ex_object, dict) and not soft:
+        return DictCheckerError
+    elif issubclass(type(ex_object), type) and not soft:
+        return TypeCheckerError
+    else:
+        return CheckerError
 
 
 @pytest.mark.parametrize('soft', [True, False])
@@ -48,8 +89,8 @@ def test_checker_positive(expected, current, soft):
     assert Checker(expected, soft).validate(current) == current
 
 
-@pytest.mark.parametrize('data', CHECKER_DATA_NEGATIVE)
-def test_checker_negative(data):
-    expected_data, soft, current_data, exception = data
-    with pytest.raises(exception):
-        Checker(expected_data, soft).validate(current_data)
+@pytest.mark.parametrize('soft', [True, False])
+@pytest.mark.parametrize(('expected', 'current'), CHECKER_DATA_NEGATIVE)
+def test_checker_negative_soft(expected, current, soft):
+    with pytest.raises(_get_expected_exception(expected, soft)):
+        Checker(expected, soft).validate(current)
