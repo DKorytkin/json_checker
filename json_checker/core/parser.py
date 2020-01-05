8e1cd322badd2d9b36894b7d60fd20a68220a515
Denis Korytkin <denis.korytkin@datarobot.com>
2020-01-05 20:42:26 +0200

    add examples to parser template

---
+++ json_checker/core/parser.py
@@ -0,0 +1,21 @@
+"""
+{
+    "id": 0 < {{int}} < 99,
+    "name": {{str}},
+    "data": {
+        "profile_id": {{int}},
+        "settings": [{{int}}]
+    }
+}
+"""
+
+"""
+{
+    "id": {"type": int, "max": 99, "min": 1}},
+    "name": {{str}},
+    "data": {
+        "profile_id": {{int}},
+        "settings": [{{int}}]
+    }
+}
+"""
